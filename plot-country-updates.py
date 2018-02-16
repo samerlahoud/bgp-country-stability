4ac80e5b909682a469d141cae6c1c61c9a7fd7f2
samerlahoud <samer@lahoud.fr>
2018-02-16 23:31:28 +0200

    update plot

--- plot-country-updates.py
+++ plot-country-updates.py
@@ -1,32 +1,39 @@
+import glob
 import matplotlib.pyplot as plt
 import csv
+import os
 
 x = []
 y = []
 z = []
+output_path = './output'
 
-with open('bgp-stability-lb-1518687353.8155603.txt','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        if int(row[2]) != 0:
-            x.append(int(row[2]))
-            y.append(int(row[1]))
-            z.append(float(int(row[1])/int(row[2])))
+for output_file in glob.glob(os.path.join(output_path, '*.txt')):
+    with open(output_file,'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=',')
+        for row in plots:
+            if row[2] == '0' or row[2] == 'None':
+                continue
+            else:
+                x.append(int(row[2]))
+                y.append(int(row[1]))
+                z.append(float(int(row[1])/int(row[2])))
 
-fig1 = plt.figure()
-plt.semilogy(x,y,'x')
-plt.xlabel('Number of prefixes')
-plt.ylabel('Number of updates')
-plt.title('BGP updates in last two weeks for Lebanese ASes')
-plt.legend()
-fig1.savefig('update-lb.png')
+    nb_update_fig = plt.figure()
+    plt.semilogy(x,y,'x')
+    plt.xlabel('Number of prefixes')
+    plt.ylabel('Number of updates')
+    plt.title('BGP updates in last two weeks')
+    plt.legend()
+    nb_update_fig.savefig(output_file.replace(".txt","")+'-nb-update.png')
+    plt.close(nb_update_fig)
 
-fig2 = plt.figure()
-plt.semilogy(x,z,'x')
-plt.xlabel('Number of prefixes')
-plt.ylabel('Update ratio')
-plt.title('BGP update ratio in last two weeks for Lebanese ASes')
-plt.legend()
-fig2.savefig('update-ratio-lb.png')
-
+    update_ratio_fig = plt.figure()
+    plt.loglog(x,z,'x')
+    plt.xlabel('Number of prefixes')
+    plt.ylabel('Update ratio')
+    plt.title('BGP update ratio in last two weeks')
+    plt.legend()
+    update_ratio_fig.savefig(output_file.replace(".txt","")+'-update-ratio.png')
+    plt.close(update_ratio_fig)
 
