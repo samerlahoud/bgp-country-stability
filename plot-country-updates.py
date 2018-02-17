899b83841a4f92241de1d408b19fa3c001e5f88f
samerlahoud <samer@lahoud.fr>
2018-02-18 00:28:29 +0200

    add nb updates

--- plot-country-updates.py
+++ plot-country-updates.py
@@ -33,11 +33,18 @@ for output_file in sorted(glob.glob(os.path.join(output_path, '*.txt'))):
         merge_nb_updates.append(np.log10(nb_updates))
         merge_update_ratio.append(np.log10(update_ratio))
 
+        nb_updates_fig = plt.figure()
+        plt.loglog(nb_prefixes,nb_updates,'x')
+        plt.xlabel('Number of prefixes')
+        plt.ylabel('Number of updates')
+        nb_updates_fig.savefig(figure_path+'nb-updates-'+country_code+'-'+timestamp+'.png')
+        plt.close(nb_updates_fig)
+        
         update_ratio_fig = plt.figure()
         plt.loglog(nb_prefixes,update_ratio,'x')
         plt.xlabel('Number of prefixes')
         plt.ylabel('Number of updates per prefix')
-        update_ratio_fig.savefig(figure_path+'nb-updates-'+country_code+'-'+timestamp+'.png')
+        update_ratio_fig.savefig(figure_path+'update-ratio-'+country_code+'-'+timestamp+'.png')
         plt.close(update_ratio_fig)
 
 fig, ax = plt.subplots()
@@ -59,22 +66,4 @@ plt.setp(ax, xticks=[y+1 for y in range(len(merge_update_ratio))],
          xticklabels=country_codes)
 fig.savefig(figure_path+'update-ratio-menog-'+timestamp+'.png')
 plt.close(fig)
-
-#    nb_update_fig = plt.figure()
-#    plt.semilogy(x,y,'x')
-#    plt.xlabel('Number of prefixes')
-#    plt.ylabel('Number of updates')
-#    plt.title('BGP updates in last two weeks')
-#    plt.legend()
-#    nb_update_fig.savefig('nb-updates'+output_file.replace(".txt","")+'.png')
-#    plt.close(nb_update_fig)
-
-#    update_ratio_fig = plt.figure()
-#    plt.loglog(x,z,'x')
-#    plt.xlabel('Number of prefixes')
-#    plt.ylabel('Update ratio')
-#    plt.title('BGP update ratio in last two weeks')
-#    plt.legend()
-#    update_ratio_fig.savefig(output_file.replace(".txt","")+'-update-ratio.png')
-#    plt.close(update_ratio_fig)
 
