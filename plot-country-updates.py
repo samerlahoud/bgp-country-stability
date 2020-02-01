f22c56ee23ac2b171590d10f6db5c69adfc1a7e3
samerlahoud <samer@lahoud.fr>
2020-02-01 09:47:41 +0200

    Update plot-country-updates.py

--- plot-country-updates.py
+++ plot-country-updates.py
@@ -6,18 +6,23 @@ import csv
 import os
 import re
 from datetime import datetime
+import seaborn as sns
 
 merge_nb_updates = []
 merge_update_ratio = []
+merge_update_ratio_per_hour = []
 merge_nb_prefixes = []
 country_codes = []
 output_path = './output/'
 figure_path = './figures/'
 
+sns.set()
+
 for output_file in sorted(glob.glob(os.path.join(output_path, '*.txt'))):
     nb_prefixes = []
     nb_updates = []
     update_ratio = []
+    update_ratio_per_hour = []
     country_code = re.split('-',output_file)[2]
 
     timestamp = '-'.join(re.split(r'[-.]',output_file)[4:7])
@@ -30,9 +35,11 @@ for output_file in sorted(glob.glob(os.path.join(output_path, '*.txt'))):
                 nb_prefixes.append(int(row[2]))
                 nb_updates.append(int(row[1]))
                 update_ratio.append(float(int(row[1])/int(row[2])))
+                update_ratio_per_hour.append(float(int(row[1])/(336*int(row[2]))))
         country_codes.append(country_code)
         merge_nb_updates.append(np.log10(nb_updates))
         merge_update_ratio.append(np.log10(update_ratio))
+        merge_update_ratio_per_hour.append(update_ratio_per_hour)
         merge_nb_prefixes.append(nb_prefixes)
 
         nb_updates_fig = plt.figure()
@@ -41,13 +48,20 @@ for output_file in sorted(glob.glob(os.path.join(output_path, '*.txt'))):
         plt.ylabel('Number of updates')
         nb_updates_fig.savefig(figure_path+'nb-updates-'+country_code+'-'+timestamp+'.png')
         plt.close(nb_updates_fig)
-        
+           
         update_ratio_fig = plt.figure()
         plt.loglog(nb_prefixes,update_ratio,'x')
         plt.xlabel('Number of prefixes')
         plt.ylabel('Number of updates per prefix')
         update_ratio_fig.savefig(figure_path+'update-ratio-'+country_code+'-'+timestamp+'.png')
-        plt.close(update_ratio_fig)
+        plt.close(update_ratio_fig) 
+
+        update_ratio_per_hour_fig = plt.figure()
+        plt.loglog(nb_prefixes,update_ratio_per_hour,'x')
+        plt.xlabel('Number of prefixes')
+        plt.ylabel('Number of updates per prefix per hour')
+        update_ratio_per_hour_fig.savefig(figure_path+'update-ratio-per-hour-'+country_code+'-'+timestamp+'.png')
+        plt.close(update_ratio_per_hour_fig)
 
 fig, ax = plt.subplots()
 ax.violinplot(merge_nb_updates, showmedians=True)
@@ -69,4 +83,12 @@ plt.setp(ax, xticks=[y+1 for y in range(len(merge_update_ratio))],
 fig.savefig(figure_path+'update-ratio-menog-'+timestamp+'.png')
 plt.close(fig)
 
+fig, ax = plt.subplots()
+g=sns.boxplot(data=merge_update_ratio_per_hour)
+ax.grid(True)
+plt.ylabel('Number of updates per prefix per hour')
+plt.yscale('log')
+g.set(xticklabels=country_codes)
+fig.savefig(figure_path+'update-ratio-per-hour-menog-'+timestamp+'.png')
+plt.close(fig)
 
